59a65adcfe36ac47f84a5257ebb2dc9692bb96b5
chauvantoan <100343041+chauvantoan@users.noreply.github.com>
2022-02-24 15:17:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    $a = 5;
+    echo $a;
+    ?>
+<?php
+    define('PI', 3.14);
+    echo PI;
+    ?>
+<?php
+    $str = "hello world";
+    echo $str;
+    ?>
+<?php
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }
+  public function message() {
+    return "My car is a " . $this->color . " " . $this->model . "!";
+  }
+}
+
+$myCar = new Car("black", "Volvo");
+echo $myCar -> message();
+echo "<br>";
+$myCar = new Car("red", "Toyota");
+echo $myCar -> message();
+?> 
